0adc2a2e5a96b87077e5c57e9928a58b7546cbce
unknown <gopal.sabhadiya@gmail.com>
2021-11-29 11:18:44 +0530

    Item add form

--- lib/widgets/body/authenticated/inventory/add_edit/top_section.dart
+++ lib/widgets/body/authenticated/inventory/add_edit/top_section.dart
@@ -1,5 +1,7 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:jb_fe/constants/colors.dart';
 import 'package:jb_fe/constants/texts/item_text.dart';
 import 'package:jb_fe/widgets/calligraphy/app_text.dart';
 import 'package:jb_fe/widgets/common/inputs/button.dart';
@@ -17,29 +19,62 @@ class ItemInputTopSection extends StatelessWidget {
           onChanged: _onPublicPrivateChange,
           hint: ItemText.PUBLIC_INPUT_TEXT,
         ),
-        Row(
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                AppButton(
-                  hint: ItemText.SELECT_IMAGE_INPUT_TEXT,
-                  onClick: _onButtonClick,
-                  colorScheme: ButtonColorScheme.BLUE,
+        IntrinsicHeight(
+          child: Row(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  AppButton(
+                    hint: ItemText.SELECT_IMAGE_INPUT_TEXT,
+                    onClick: _onButtonClick,
+                    colorScheme: ButtonColorScheme.BLUE,
+                  ),
+                  AppButton(
+                    hint: ItemText.CLEAR_IMAGES_INPUT_TEXT,
+                    onClick: _onButtonClick,
+                    colorScheme: ButtonColorScheme.RED,
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: Opacity(
+                  opacity: 0.5,
+                  child: DottedBorder(
+                      dashPattern: const [4, 3],
+                      strokeWidth: 2,
+                      strokeCap: StrokeCap.butt,
+                      borderType: BorderType.RRect,
+                      radius: const Radius.circular(8),
+                      padding: const EdgeInsets.all(6),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: AppColors.grey_2,
+                            borderRadius: BorderRadius.circular(8)),
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 5,
+                          horizontal: 60,
+                        ),
+                        child: Column(
+                          children: [
+                            Icon(
+                              Icons.cloud_upload,
+                              size: 65,
+                              color: AppColors.blue_5,
+                            ),
+                            AppTextBuilder("Drop Image Here")
+                                .size(20)
+                                .color(AppColors.blue_5)
+                                .build()
+                          ],
+                        ),
+                      )),
                 ),
-                AppButton(
-                  hint: ItemText.CLEAR_IMAGES_INPUT_TEXT,
-                  onClick: _onButtonClick,
-                  colorScheme: ButtonColorScheme.RED,
-                ),
-              ],
-            ),
-            Container(
-              width: 20,
-              height: 20,
-              // decoration: BoxDecoration,
-            )
-          ],
+              ),
+            ],
+          ),
         ),
         AppTextBuilder("UploadedImages").build()
       ],
